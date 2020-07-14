26077935a0640c612ff56d802b8e2560d7b570bc
RainingComputers <vishnu.vish.shankar@gmail.com>
2020-07-14 19:12:17 +0530

    Changed from GB to GiB for memory

--- websysmon/monitor/bytes2human.py
+++ websysmon/monitor/bytes2human.py
@@ -1,12 +1,6 @@
 # Utility function to convert number of bytes to string
 def bytes2human(nbytes):
-    if(nbytes < 1000): 
-        return str(nbytes)+' Bytes'
-    elif(nbytes < (1000**2)):
-        return str(round(nbytes/1000, 2))+' KB'
-    elif(nbytes < (1000**3)):
-        return str(round(nbytes/(1000**2), 2))+' MB'
-    elif(nbytes < (1000**4)):
-        return str(round(nbytes/(1000**3), 2))+' GB'
-    else:
-        return str(round(nbytes/(1000**4), 2))+' TB'
+    if(nbytes < (1024*1024*1024)):
+        return str(round(nbytes/(1024*1024), 2))+' MiB'
+    else :
+        return str(round(nbytes/(1024*1024*1024), 2))+' GiB'
